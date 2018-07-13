37ef8086af049ed32c974ea9a87f29a3d5ca8a45
Unknown <delltadragon@gmail.com>
2018-07-13 16:28:30 -0400

    Add all the things
    Adding the main dir and the resorces.

---
+++ src/main/java/com/mightymainframe/dellta/uhcrevamped/UHCRevamped.java
@@ -0,0 +1,14 @@
+package com.mightymainframe.dellta.uhcrevamped;
+
+import org.bukkit.plugin.java.*;
+
+public final class UHCRevamped extends JavaPlugin {
+	
+	public void onEnable() {
+		// TODO Insert Logic to be preformed when the plugin is enabled
+	}
+	
+	public void onDisable() {
+		// TODO Insert logic to be preformed when the plugin is disabled
+	}
+}
